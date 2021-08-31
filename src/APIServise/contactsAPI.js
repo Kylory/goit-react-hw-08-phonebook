99513a7a3dcf1c8da5dbe0df001e52e5fd9bf2a4
Kyrylo Moskalenko <krllmsk@gmail.com>
2021-08-31 20:55:34 +0300

    delete comments

--- src/APIServise/contactsAPI.js
+++ src/APIServise/contactsAPI.js
@@ -1,26 +1,13 @@
 import axios from "axios";
-// import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// const BASE_URL = 'https://connections-api.herokuapp.com';
 axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
-// const token = {
-//   set(token) {
-//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-//   },
-//   unset() {
-//     axios.defaults.headers.common.Authorization = '';
-//   },
-// };
-
 async function fetchContacts() {
-  // const { data } = await axios.get(`${BASE_URL}/contacts`);
   const { data } = await axios.get("/contacts");
   return data;
 }
 
 async function postContact(name, number) {
-  // const { data } = await axios.post(`${BASE_URL}/contacts`, {
   const { data } = await axios.post("/contacts", {
     name: `${name}`,
     number: `${number}`,
@@ -29,7 +16,6 @@ async function postContact(name, number) {
 }
 
 async function deleteContact(id) {
-  // await axios.delete(`${BASE_URL}/contacts/${id}`);
   await axios.delete(`/contacts/${id}`);
 
   return id;
--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,4 +1,3 @@
-// import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { authSelectors } from "redux/auth";
 import AuthNav from "../AuthNav/AuthNav";
